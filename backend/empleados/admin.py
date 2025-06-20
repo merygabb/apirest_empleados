90bc0bffb03b95095eda64bd73b94d3760e62a3c
merygabb <gabybatzpinto@gmail.com>
2025-06-20 01:54:21 -0600

    registro del modelo empleado en el admin de Drf

--- backend/empleados/admin.py
+++ backend/empleados/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+# regitra modelos al administrador de Django
+from django.contrib import admin
+from .models import Empleado
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre', 'estado')
+    search_fields = ('codigo', 'nombre')
+    list_filter = ('estado',)
+    ordering = ('codigo',)
+    fieldsets = (
+        (None, {
+            'fields': ('codigo', 'nombre', 'estado')
+        }),
+    )
